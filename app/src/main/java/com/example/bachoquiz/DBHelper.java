555b6c8f693361c0d93e1871bd4660602c8b59d9
mzaman21 <zwordpressservices@gmail.com>
2023-01-15 18:50:05 +0500

    Add DBHelper Class

--- app/src/main/java/com/example/bachoquiz/DBHelper.java
+++ app/src/main/java/com/example/bachoquiz/DBHelper.java
@@ -1,4 +1,30 @@
 package com.example.bachoquiz;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String id = "Id";
+    public static final String Option_No = "OptionNo";
+    public static final String Correct_Ans = "CorrectAns";
+    public static final String Given_Ans = "GivenAns";
+    public static final String Result = "Result";
+
+    public DBHelper(Context context) {
+        super(context, "OnlineTestDB.db",null, 4);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
